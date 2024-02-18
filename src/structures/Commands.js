4e1d698a3228e6b4a544778bdaa81ce8217ae342
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-02-18 11:44:22 -0300

    Update Commands.js
    # Changes
    Improved and optimized code

--- src/structures/Commands.js
+++ src/structures/Commands.js
@@ -6,22 +6,23 @@ import { token, botId } from '../../config.js'
 export const CommandHandler = async (client, roothPath) => {
     const AllFiles = await Filereader(`${roothPath}/src/commands`);
     const rest = new REST({ version: '10' }).setToken(token);
-    const CommandsArray = [];
     
     if (AllFiles.length > 0) {
-        for (const CommandFile of AllFiles) {
-            const { Command } = await import(pathToFileURL(CommandFile))
+        const commandsPromises = AllFiles.map(async (CommandFile) => {
+            const { Command } = await import(pathToFileURL(CommandFile));
             if (Command && !Command.ignore && Command.name && Command.description) {
                 client.slashCommands?.set(Command.name, Command);
-                CommandsArray.push({
+                return {
                     name: Command.name,
                     nsfw: Command.nsfw ?? false,
                     description: Command.description,
                     type: ApplicationCommandType.ChatInput,
                     options: Command.options ?? []
-                });
+                };
             }
-        }
+        });
+
+        const CommandsArray = (await Promise.all(commandsPromises)).filter(Boolean);
 
         try {
             console.log('Started refreshing application (/) commands.');
