e184ceedbdb2e51e18ac4030904f0bec12c6da50
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-02-18 11:44:44 -0300

    Update Events.js
    2x faster now

--- src/structures/Events.js
+++ src/structures/Events.js
@@ -1,24 +1,31 @@
-import { Filereader } from '../utils/filereader.js'
-import { pathToFileURL } from 'node:url'
+import { Filereader } from '../utils/filereader.js';
+import { pathToFileURL } from 'node:url';
 
-export const EventHandler = async(client, rootPath) => {
+export const EventHandler = async (client, rootPath) => {
     const eventFiles = Filereader(`${rootPath}/src/events`);
     if (!eventFiles.length) return;
 
-    await Promise.all(eventFiles.map(async (event) => {
-        try {
-            const clientEvent = (await import(pathToFileURL(event)))?.Event;
-            if (clientEvent) {
-                client.events?.set(clientEvent.name, clientEvent);
+    const promises = [];
+    for (const event of eventFiles) {
+        promises.push(
+            (async () => {
+                try {
+                    const { Event: clientEvent } = await import(pathToFileURL(event)) || {};
+                    if (clientEvent) {
+                        client.events?.set(clientEvent.name, clientEvent);
 
-                if (!clientEvent.ignore) {
-                    if (clientEvent.customEvent) clientEvent.run(client);
-                    else if (clientEvent.runOnce) client.once(clientEvent.name, (...args) => clientEvent.run(...args, client));
-                    else client.on(clientEvent.name, (...args) => clientEvent.run(client, ...args));
+                        if (!clientEvent.ignore) {
+                            if (clientEvent.customEvent) clientEvent.run(client);
+                            else if (clientEvent.runOnce) client.once(clientEvent.name, (...args) => clientEvent.run(...args, client));
+                            else client.on(clientEvent.name, (...args) => clientEvent.run(client, ...args));
+                        }
+                    }
+                } catch (error) {
+                    console.error(error);
                 }
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }));
+            })()
+        );
+    }
+
+    await Promise.allSettled(promises);
 };
